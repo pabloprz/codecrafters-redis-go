0895e356945fdf06bed9782b6d259c980f24d58e
Pablo Perez <pablo.perez.rodriguez@udc.es>
2024-11-03 13:09:36 -0600

    responding PONG

--- app/server.go
+++ app/server.go
@@ -7,14 +7,39 @@ import (
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+
+	fmt.Println("started redis server on port 6379")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		handleClientConn(conn)
 	}
+}
+
+func handleClientConn(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Printf("new connection from %s\n", conn.LocalAddr().String())
+
+	buffer := make([]byte, 1028)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
+		return
+	}
+
+	fmt.Printf("Received %d bytes %b", n, buffer[:n])
+
+	conn.Write([]byte("+PONG\r\n"))
 }
